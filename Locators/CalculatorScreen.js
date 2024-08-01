ba17ad0f1daa43136d978d5069d0a0e8b33f4088
Harish5351 <harishekal21496@gmail.com>
2024-08-01 10:37:31 +0530

    locators

---
+++ Locators/CalculatorScreen.js
@@ -0,0 +1,7 @@
+module.exports = {
+    tap_four: $('//android.widget.Button[@text="4"]'),
+    tap_add: $('//android.widget.Button[@text="+"]'),
+    tap_equal: $('//android.widget.Button[@text="="]'),
+    verify_result:$('//android.widget.TextView[@resource-id="com.miui.calculator:id/result"]')
+    // successMessage: $("//h3[normalize-space()='XXX']")
+}
